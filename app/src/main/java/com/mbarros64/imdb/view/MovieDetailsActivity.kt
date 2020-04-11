a16f0cf6b4d45e29d2aec81b5c20254cdf8b643f
matheus <barros.matheusprestes@gmail.com>
2020-04-11 18:58:31 -0400

    Definindo o novo activity para detalhes dos filmes
    Signed-off-by: matheus <barros.matheusprestes@gmail.com>

---
+++ app/src/main/java/com/mbarros64/imdb/view/MovieDetailsActivity.kt
@@ -0,0 +1,13 @@
+package com.mbarros64.imdb.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mbarros64.imdb.R
+
+class MovieDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_details)
+    }
+}
